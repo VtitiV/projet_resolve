33c0bd007aeafe2725d87e113fc91289ff79e1ce
tbrochard <thierrybrochard6@gmail.com>
2021-08-30 14:23:29 +0200

    user.csv

--- Demande/views.py
+++ Demande/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def demandedepart(request):
     return render(request, "demandetemplates/demande.html")
 
+def demande_de_compte(request):
+    return render(request, "demandetemplates/comptes/choixtypedecomptes.html")
+
 
