6fe6e6f32399718a3e042781a683f68c794cf66c
tbrochard <thierrybrochard6@gmail.com>
2021-08-30 14:23:29 +0200

    user.csv

--- Demande/urls.py
+++ Demande/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import demandedepart
+from django.urls import path, include
+from .views import demandedepart, demande_de_compte
 
 urlpatterns = [
     path('',demandedepart, name="demandedepart"),
+    path('demande_de_compte/',demande_de_compte, name="demande_de_compte"),
 
 ]
